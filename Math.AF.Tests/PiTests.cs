7621f249564ff3244002c2ae28c44fc68c1de378
Ardy Foolen <ardyfoolen@abv.bg>
2022-11-17 15:33:21 +0200

    Add project files.

---
+++ Math.AF.Tests/PiTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math.AF.Tests
+{
+    public class PiTests
+    {
+        [Test]
+        public void NotARealTest()
+        {
+            var bytes = $"3.{string.Concat(Pi.PiDigits(0x100).Take(100).Select(_ => $"{_.ToString("x2")} "))}";
+            var words = $"3.{string.Concat(Pi.PiDigits(0x10000).Take(100).Select(_ => $"{_.ToString("x4")} "))}";
+            var dwords = $"3.{string.Concat(Pi.PiDigits(0x100000000).Take(100).Select(_ => $"{_.ToString("x8")} "))}";
+            var result10 = $"3.{string.Concat(Pi.PiDigits().Take(100).Select(_ => _.ToString("d")))}";
+            var result16 = $"3.{string.Concat(Pi.PiDigits(16).Take(100).Select(_ => _.ToString("x")))}";
+            var blowfishKey = $"{string.Concat(Pi.PiDigits(16).Take(144).Select(_ => _.ToString("x")))}";
+            var blowfishS1 = $"{string.Concat(Pi.PiDigits(16).Skip(144).Take(2048).Select(_ => _.ToString("x")))}";
+            var blowfishS2 = $"{string.Concat(Pi.PiDigits(16).Skip(2192).Take(2048).Select(_ => _.ToString("x")))}";
+            var blowfishS3 = $"{string.Concat(Pi.PiDigits(16).Skip(4240).Take(2048).Select(_ => _.ToString("x")))}";
+            var blowfishS4 = $"{string.Concat(Pi.PiDigits(16).Skip(6288).Take(2048).Select(_ => _.ToString("x")))}";
+        }
+
+        private ulong[] HexPiDigits = new ulong[]
+        {
+            0x2,0x4,0x3,0xF,0x6,0xA,0x8,0x8,0x8,0x5,0xA,0x3,0x0,0x8,0xD,0x3,0x1,0x3,0x1,0x9,0x8,0xA,0x2,0xE,0x0,0x3,0x7,0x0,0x7,0x3
+        };
+
+        private ulong[] DecPiDigits = new ulong[]
+        {
+            0x1,0x4,0x1,0x5,0x9,0x2,0x6,0x5,0x3,0x5,0x8,0x9,0x7,0x9,0x3,0x2,0x3,0x8,0x4,0x6,0x2,0x6,0x4,0x3,0x3,0x8,0x3,0x2,0x7,0x9
+        };
+
+        [Test]
+        public void PiDigits_Hex_Succeeds()
+        {
+            // Act
+            var result = Pi.PiDigits(16).Take(30).ToArray();
+
+            // Assert
+            Assert.That(result, Is.EquivalentTo(HexPiDigits));
+        }
+
+        [Test]
+        public void PiDigits_Dec_Succeeds()
+        {
+            // Act
+            var result = Pi.PiDigits(10).Take(30).ToArray();
+
+            // Assert
+            Assert.That(result, Is.EquivalentTo(DecPiDigits));
+        }
+    }
+}
---
+++ Math.AF/Pi.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math.AF
+{
+    public class Pi
+    {
+        // Convert Pi to Pi in hex
+        // Link: https://math.stackexchange.com/questions/1231826/how-to-convert-pi-to-base-16
+        // π = 3.141592653589793238462643383279
+        // in hex = 3.243F6A8885A308D313198A2E037073
+
+        // a0 = 3.141592653589793238462643383279 = 3
+        // a1 = 16 * .1415926535897932384626433832795 = 2
+        // a2 = 16 * (a1 result - 2) = 4
+        // a3 = 16 * (a2 result - 4) = 3
+        // a4 = 16 * (a3 result - 3) = 15 = F
+        // a5 = 16 * (a4 result - 15) = 6
+        // a6 = 16 * (a5 result - 6) = 10 = A
+        // a7 = 16 * (a6 result - 10) = 8
+        // a8 = 16 * (a7 result - 8) = 8
+        // a9 = 16 * (a8 result - 8) = 8
+        // a10 = 16 * (a9 result - 8) = 5
+        // a11 = 16 * (a10 result - 5) = A
+
+        /// <summary>Enumerates the digits of PI.</summary>
+        /// Link: https://rosettacode.org/wiki/Pi#C#
+        /// <param name="base">Base of the Numeral System to use for the resulting digits (default = Base.Decimal (10)).</param>
+        /// <returns>The digits of PI.</returns>
+        public static IEnumerable<long> PiDigits(long @base = 10)
+        {
+            BigInteger
+              k = 1,
+              l = 3,
+              n = 3,
+              q = 1,
+              r = 0,
+              t = 1
+              ;
+
+            // skip integer part
+            var nr = @base * (r - t * n);
+            n = @base * (3 * q + r) / t - @base * n;
+            q *= @base;
+            r = nr;
+
+            for (; ; )
+            {
+                var tn = t * n;
+                if (4 * q + r - t < tn)
+                {
+                    yield return (long)n;
+                    nr = @base * (r - tn);
+                    n = @base * (3 * q + r) / t - @base * n;
+                    q *= @base;
+                }
+                else
+                {
+                    t *= l;
+                    nr = (2 * q + r) * l;
+                    var nn = (q * (7 * k) + 2 + r * l) / t;
+                    q *= k;
+                    l += 2;
+                    ++k;
+                    n = nn;
+                }
+                r = nr;
+            }
+        }
+
+        public static IEnumerable<uint> HexPiToUints(int skip, int take)
+            => Pi.PiDigits(16)
+            .Skip(skip * 8)
+            .Take(take * 8)
+            .Chunk(8)
+            .Select(m => m.Aggregate((uint)0x00000000, (acc, a) => acc << 4 | (byte)a));
+
+    }
+}
